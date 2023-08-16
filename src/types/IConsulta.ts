acf61ccd371360aa7fefc3d84cae80686c97e022
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-16 14:57:39 -0300

    useFetch

---
+++ src/types/IConsulta.ts
@@ -0,0 +1,12 @@
+import IProfissional from './IProfissional';
+
+
+export default interface IConsulta extends IProfissional {
+    id: number,
+    data: string,
+    horario: string,
+    profissional: Array<IProfissional>
+    especialidade: string,
+    paciente: string,
+    modalidade: string
+}
