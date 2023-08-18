61a06d9048ed30d39fd09a273d73d4ac0bc874e0
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-18 07:33:25 -0300

    Avaliacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import Avaliacao from './components/Avaliacao';
+import Botao from './components/Botao';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
 import Grafico from './components/Grafico';
 import Rodape from './components/Rodape';
+import Subtitulo from './components/Subtitulo';
 import Tabela from './components/Tabela';
 import Titulo from './components/Titulo';
 import useDadosConsulta from './useDadosConsulta';
@@ -15,17 +18,23 @@ function App() {
   if (consultasErro || profissionaisErro) {
     console.log("Ocorreu um erro na requisição")
   }
+
   return (
     <>
       <Cabecalho />
       <Container>
-        <Titulo>Área administrativa</Titulo>
+        <Titulo>Área Administrativa</Titulo>
+        <Botao>Cadastrar especialista</Botao>
+        <Titulo imagem="consulta">Consultas do Dia</Titulo>
         <Tabela consultas={consultas} />
+        <Titulo imagem="grafico">Consultas mensais por especialista</Titulo>
+        <Subtitulo>Dezembro/22</Subtitulo>
         <Grafico consultas={consultas} profissionais={profissionais} />
+        <Titulo imagem="avaliacao">Avaliações de especialistas</Titulo>
+        <Avaliacao profissionais={profissionais} />
       </Container>
       <Rodape />
     </>
-    
   );
 }
 
